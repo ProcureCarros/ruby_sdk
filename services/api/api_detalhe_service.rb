44c8a1f7d52148634a20e9001f223f8394b8dc78
Iuri Andreazza <iuri.andreazza@gmail.com>
2015-07-04 23:45:29 -0300

    - Ajuste no SDK de lista - Ajuste no SDK do detalhe

--- services/api/api_detalhe_service.rb
+++ services/api/api_detalhe_service.rb
@@ -8,17 +8,51 @@ module Procurecarros
       # @@endpoint = ServiceLayer::ApiService::DETALHE_ENDPOINT
 
       def initialize client_id, client_token
-        @client = Procurecarros::ServiceLayer::Client.discover("v1","lista", client_id, client_token)
+        @client = Procurecarros::ServiceLayer::Client.discover("v1","detalhe", client_id, client_token)
       end
 
-      def getDetalheAnuncio(url)
-        method = @client.api.detalhe.get_detalhe
+      def getDetalheAnuncio(codigoAnuncio)
+        method = @client.api.get_detalhe
         params = {
-
+          :codigoAnuncio => codigoAnuncio
         }
         return @client.query(method, params)
       end
 
+
+      def sendLead(codigoAnuncio, form)
+        method = @client.api.send_lead
+        params = {
+          :codigo_anuncio => codigoAnuncio,
+          :nome => form[:nome],
+          :email => form[:email],
+          :telefone => form[:telefone],
+          :msg => form[:msg],
+          :ofertas_similares => form[:ofertas_similares],
+          :newsletter => form[:newsletter]
+        }
+        return @client.query(method, params)
+      end
+
+      def getSimulacaoFinanciamento(valor_automovel, entrada, parcelas, txJuros)
+        method = @client.api.simulacao_financiamento
+        params = {
+            :valor_automovel => valor_automovel,
+            :entrada => entrada,
+            :parcelas => parcelas,
+            :txJuros => txJuros
+        }
+        return @client.query(method, params)
+      end
+
+      def getValorIPVA(estadoSigla, valor_veiculo)
+        method = @client.api.valor_ipva
+        params = {
+            :estado => estadoSigla,
+            :valor_veiculo => valor_veiculo
+        }
+        return @client.query(method, params)
+      end
 
     end
   end
--- services/api/api_lista_service.rb
+++ services/api/api_lista_service.rb
@@ -14,12 +14,30 @@ module Procurecarros
         @client = Procurecarros::ServiceLayer::Client.discover("v1","lista", client_id, client_token)
       end
 
-      def search(search_params, curr_page, num_reg = 50)
+      ##
+      #
+      #
+      # @param search_params => {
+      #   :termo_pesquisa => termo_pesquisa || nil,
+      #   :uf => uf || nil,
+      #   :nome_cidade => nome_cidade || nil,
+      #   :marca => marca || nil,
+      #   :modelo => modelo || nil,
+      #   :ano_min_max => ano_min_max || nil,
+      #   :nome_versao => nome_versao || nil,
+      #   :cor => cor || nil,
+      #   :km_min_max => km_min_max || nil,
+      #   :preco_min_max => preco_min_max || nil
+      # }
+      # @param page => 1,2,3,4
+      # @param num_reg => 50,100,200
+      #
+      def search(search_params, page, num_reg = 50)
 
         method = @client.api.search
         params = {
             "search_params" => search_params,
-            "page" => curr_page,
+            "page" => page,
             "registros" => num_reg
         }
         return @client.query(method, params)
