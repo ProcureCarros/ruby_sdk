b53b66f263e12ef096424485aaaea1b48909a7bc
Iuri Andreazza <iuri.andreazza@gmail.com>
2015-09-30 10:42:33 -0300

    adding new API endpoint - User Service

---
+++ services/api/api_usuario_service.rb
@@ -0,0 +1,37 @@
+module Procurecarros
+  module ServiceLayer
+
+    require('services/client')
+
+    ##
+    # Query Service Layer for data
+    #
+    #
+    class ApiUsuarioService
+
+      def initialize client_id, client_token
+        @client = Procurecarros::ServiceLayer::Client.discover("v1","usuario", client_id, client_token)
+      end
+
+      def authenticate(usuario, password)
+        #TODO: sha1 on password
+        method = @client.api.login
+        params = {
+            "usuario" => usuario,
+            "password" => password
+        }
+        return @client.query(method, params)
+      end
+
+      def logout(usuario)
+        method = @client.api.logout
+        params = {
+            "usuario" => usuario
+        }
+        return @client.query(method, params)
+      end
+
+    end
+  end
+
+end
